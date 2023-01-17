531dd6698ffaaeaffde7b69bd2bac180ba1678d8
wilfredo-flores <willflores1221@gmail.com>
2023-01-17 00:10:26 -0500

    second init

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# TO-DO
+
+[] switch background to rtc
+[] optimize image files sizes (webp)
+
 # Welcome to [Astro](https://astro.build)
 
 [![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/withastro/astro/tree/latest/examples/basics)
@@ -6,7 +11,6 @@
 > 🧑‍🚀 **Seasoned astronaut?** Delete this file. Have fun!
 
 ![basics](https://user-images.githubusercontent.com/4677417/186188965-73453154-fdec-4d6b-9c34-cb35c248ae5b.png)
-
 
 ## 🚀 Project Structure
 
---
+++ src/const.js
@@ -0,0 +1,57 @@
+// SEO RULES titles < 50 characters, descriptions < 150 characters
+export const COMPANY = "Auto Body Shop Rockville";
+export const EMAIL = "info@email.com";
+export const ADDRESS = "1621 East Gude Dr, Rockville, MD 20850";
+export const GOOGLE_MAPS_ADDRESS =
+	"https://maps.google.com/?q=1621+E+Gude+Dr+Rockville+MD+20850";
+export const LOCATION = "Rockville";
+export const LOCATION_STATE = "MD";
+export const PHONE_NUMBER = "0000000000";
+export const PHONE_NUMBER_FORMATTED = "(000) 000-0000";
+
+export const SITE_TITLE = `Auto Body ${LOCATION}. Paint, Dent, Collision Repair`; // 500
+export const SITE_DESCRIPTION = `Auto Body Shop in ${LOCATION}, ${LOCATION_STATE}. Call ${PHONE_NUMBER_FORMATTED}. Paint Repair, Collision Repair, Dent Removal, Fender Repair, Bumper Repair, and Scratch Repair.`; // 149
+
+export const ABOUT_US_PAGE = {
+	title: "About Us. Paint, Dent, Collision, Bumper Repair",
+	description:
+		"Auto Body Shop Rockville, MD. Specialize in Paint, Dent, Collision, Bumper Repair. European, Asian, or Domestic car, truck, or SUV, service them all.", // 149
+};
+
+export const SERVICES_PAGE = {
+	title: "Services Rockville. Paint, Dent, Collision, Repair", // 50
+	description:
+		"Car Repair on all makes and models for Rockville, Maryland. Paint, Dent, Collision, Bumper Repair. European, Asian, or Domestic car, truck, or SUV.", // 147
+	content: [
+		{
+			title: "AUTO BODY PAINT REPAIR",
+			description:
+				"Our team of experts have the skill and experience to repair any type of damage, from small scratches to major bodywork.",
+		},
+		{
+			title: "COLLISION REPAIR",
+			description:
+				"From minor dings to major accidents. We offer professional and comprehensive collision repair services.",
+		},
+		{
+			title: "AUTO DENT REMOVAL",
+			description:
+				"We understand the frustration of unsightly dents on your vehicle. We offer professional and efficient dent removal services.",
+		},
+		{
+			title: "FENDER REPAIR",
+			description:
+				"Bumper/fender collision? We'll fix any damage to the fenders of a vehicle.",
+		},
+		{
+			title: "BUMPER REPAIR",
+			description:
+				"Bumpers play an important role in minimizing body damage. But they also take the brunt of the impact. We specialize in repairing damage to the bumpers of vehicles.",
+		},
+		{
+			title: "SCRATCH REPAIR",
+			description:
+				"Paint scratches, Clear coat scratches, primer scratches, we specialize in scratch repair.",
+		},
+	],
+};
