fb56559d31bc4e196991f6f528075ea50758dd76
wilfredo-flores <willflores1221@gmail.com>
2023-01-18 02:12:46 -0500

    remove tw & image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -10,5 +10,5 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+	integrations: [,],
 });
