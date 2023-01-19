ecc39d3932cdd33fa00c04963794fe17e745c4c2
wilfredo-flores <willflores1221@gmail.com>
2023-01-18 19:36:41 -0500

    Third commit

--- src/const.js
+++ src/const.js
@@ -57,7 +57,68 @@ export const SERVICES_PAGE = {
 };
 
 export const CONTACT_US_PAGE = {
-	title: "About Us. Paint, Dent, Collision, Bumper Repair",
+	title: "Contact Us. Paint, Dent, Collision, Bumper Repair",
 	description:
 		"Auto Body Shop Rockville, MD. Specialize in Paint, Dent, Collision, Bumper Repair. European, Asian, or Domestic car, truck, or SUV, service them all.", // 149
+};
+
+export const IF_YOU_NEED_CTA = [
+	{
+		icon: "/assets/gear.svg",
+		title: "Collision Repairs",
+		description:
+			"If a vehicle is involved in an accident, it may require repairs to the body, frame and/or mechanical systems. Our auto body shop can repair any damage to the vehicle and restore it to its pre-accident condition",
+	},
+	{
+		icon: "/assets/gear.svg",
+		title: "Paintless Dent Repair",
+		description:
+			"Minor dents and dings can often be repaired without the need for repainting, using specialized tools and techniques",
+	},
+	{
+		icon: "/assets/gear.svg",
+		title: "Painting and Refinishing",
+		description:
+			"If a vehicle's paint is damaged, faded, or scratched, our auto body shop can repaint and refinish the vehicle to give it a new and refreshing look",
+	},
+	{
+		icon: "/assets/gear.svg",
+		title: "Rust Repair",
+		description:
+			"A vehicle has rust on the body, our auto body shop can repair the rust, and prevent it from spreading further",
+	},
+	{
+		icon: "/assets/gear.svg",
+		title: "Glass Replacement",
+		description:
+			"Our auto body shop can replace any broken or cracked glass on the vehicle, including windshields, windows, and mirrors",
+	},
+	{
+		icon: "/assets/gear.svg",
+		title: "Detailing",
+		description:
+			"We offer detailing services to clean and protect the exterior and interior of the vehicle",
+	},
+	{
+		icon: "/assets/gear.svg",
+		title: "Custom Work",
+		description:
+			"Our auto body shop also does custom work to enhance the look of your vehicle",
+	},
+];
+
+export const FAQ_PAGE = {
+	title: "Have a question about your vehicle? Click here.",
+	description:
+		"Uncertain of the price of a car repair? Do you have a question about damage to your vehicle? The most frequently asked questions, answered below.",
+	headline: "FREQUENTLY ASKED QUESTIONS",
+	FAQ_PAGES: [
+		{
+			slug: "how-much-is-full-body-work-on-a-car",
+			pathname: "/how-much-is-full-body-work-on-a-car",
+			question: "How much is full body work on a car?",
+			answer:
+				"Unforeseen events might result in auto body damage, which makes finding the best repair shop more difficult. Finding the best repair facility is essential after you notice dents or other issues with your paint or auto body. Auto body shops evaluate body damage using a variety of criteria. The repair business examines your car before beginning the repair process. They can now see the genuine auto body damage thanks to this. Once it is located, the proper repair procedure is established, and the repair process starts. Different techniques are employed by auto body shops to fix dents and other damage. A trustworthy auto body shop will guide you through the full repair procedure. Choosing the ideal auto body shop is one of the most essential parts of the process. Ok now that that's out of the way, let's answer the question. Parts, labour, and other costs might cost anything from $100 to hundreds of dollars. You can research the cost of replacing the damaged parts, the length of the repair procedure, and the labour cost (say $90/hr) to get a general idea of how much it will cost to fix your car. Car bumpers typically cost between $500 and $1,500, and painting a damaged fender costs between $500 and $2,500.",
+		},
+	],
 };
---
+++ src/faq/how-much-is-full-body-work-on-a-car.md
@@ -0,0 +1,21 @@
+---
+# layout: "../../layouts/PostLayout.astro"
+category: blog
+slug: how-much-is-full-body-work-on-a-car
+title: How much is full body work on a car?
+date: 2020-11-05 11:20:38
+description: While auto body restoration costs can vary greatly. For a good repair work, you should budget anywhere from $75 to $2,500 on average.
+draft: false
+---
+
+## Before we answer this it will help to understand:
+
+Unforeseen events might result in auto body damage, which makes finding the best repair shop more difficult. Finding the best repair facility is essential after you notice dents or other issues with your paint or auto body. Auto body shops evaluate body damage using a variety of criteria. The repair business examines your car before beginning the repair process. They can now see the genuine auto body damage thanks to this.
+
+Once it is located, the proper repair procedure is established, and the repair process starts. Different techniques are employed by auto body shops to fix dents and other damage. A trustworthy auto body shop will guide you through the full repair procedure. Choosing the ideal auto body shop is one of the most essential parts of the process.
+
+## Ok now that that's out of the way, let's answer the question
+
+Parts, labour, and other costs might cost anything from $100 to hundreds of dollars.
+
+You can research the cost of replacing the damaged parts, the length of the repair procedure, and the labour cost (say $90/hr) to get a general idea of how much it will cost to fix your car. Car bumpers typically cost between $500 and $1,500, and painting a damaged fender costs between $500 and $2,500.
