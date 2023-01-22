f23905d24f05e96b474fcdbdf4353b6d038c442b
wilfredo-flores <willflores1221@gmail.com>
2023-01-22 00:20:24 -0500

    add menu for mobile vp

---
+++ src/components/MobileMenu.svelte
@@ -0,0 +1,77 @@
+<script>
+	import { fly } from "svelte/transition";
+
+	export let show = false;
+</script>
+
+{#if show}
+	<nav
+		class="menu_wrapper"
+		transition:fly={{ x: 1000, opacity: 0.9, duration: 750 }}
+	>
+		<ul>
+			<li>
+				<button on:click={() => (show = !show)} class="close">
+					<div>X</div>
+				</button>
+			</li>
+			<li class="list_item"><a href="/"><div>Home</div></a></li>
+			<li class="list_item"><a href="/services"><div>Services</div></a></li>
+			<li class="list_item"><a href="/contactus"><div>Contact Us</div></a></li>
+			<li class="list_item"><a href="/faq"><div>FAQ</div></a></li>
+		</ul>
+	</nav>
+{/if}
+
+<style>
+	.menu_wrapper {
+		padding: 0;
+		margin: 0;
+		position: fixed;
+		top: 0;
+		right: 0;
+		height: 100%;
+		border-left: 1px solid #aaa;
+		background: #fff;
+		overflow-y: auto;
+		width: 100%;
+	}
+	ul {
+		list-style: none;
+		padding: 0;
+		margin: 0;
+	}
+	.list_item {
+		padding: 2rem;
+		text-transform: uppercase;
+		cursor: pointer;
+	}
+	.list_item:hover > a {
+		color: #ff0000;
+	}
+	.list_item {
+		padding: 0;
+		border-bottom: 1px solid black;
+	}
+	a {
+		text-decoration: none;
+		color: black;
+	}
+	a > div {
+		padding: 3rem 1.5rem;
+		font-size: 2.5rem;
+		font-weight: 500;
+	}
+	.close {
+		background-color: black;
+		color: white;
+		padding: 0;
+		margin: 0;
+		width: 100%;
+		padding: 3rem 0px;
+		cursor: pointer;
+	}
+	.close:active {
+		background-color: blue;
+	}
+</style>
---
+++ src/components/ToggleMenu.svelte
@@ -0,0 +1,42 @@
+<script>
+	import MobileMenu from "./MobileMenu.svelte";
+	let sidebar_show = false;
+
+	const toggle = () => {
+		sidebar_show = !sidebar_show;
+		console.log("coc");
+	};
+</script>
+
+<div class="mobile_menu">
+	<div class="logo">hello world</div>
+	<button on:click={toggle}>Toggle Sidebar</button>
+
+	<MobileMenu bind:show={sidebar_show} />
+</div>
+
+<style>
+	.mobile_menu {
+		padding: 0 0.25rem;
+		margin: 0;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		background-color: #1a1a1b;
+	}
+	.logo {
+		color: white;
+	}
+	button {
+		cursor: pointer;
+		padding: 0;
+		margin: 0;
+		padding: 1.5rem;
+	}
+
+	@media (min-width: 768px) {
+		.mobile_menu {
+			display: none;
+		}
+	}
+</style>
