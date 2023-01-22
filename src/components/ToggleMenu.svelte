59fb1ba75cd87daddc3dd14164e1ef8765a565ac
wilfredo-flores <willflores1221@gmail.com>
2023-01-22 13:59:03 -0500

    Add menu icon

--- src/components/ToggleMenu.svelte
+++ src/components/ToggleMenu.svelte
@@ -1,28 +1,43 @@
 <script>
 	import MobileMenu from "./MobileMenu.svelte";
+	import icon from "../../public/assets/car_logo.svg";
+	import bars from "../../public/assets/menu.svg";
+
 	let sidebar_show = false;
 
 	const toggle = () => {
 		sidebar_show = !sidebar_show;
-		console.log("coc");
 	};
 </script>
 
 <div class="mobile_menu">
-	<div class="logo">hello world</div>
-	<button on:click={toggle}>Toggle Sidebar</button>
+	<div class="logo"><img src={icon} alt="logo" height="30" width="30" /></div>
+	<button on:click={toggle}>
+		<div class="hamburger_menu">
+			<svg
+				width="32"
+				height="32"
+				viewBox="0 0 18 10"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<line y1="9.25" x2="18" y2="9.25" stroke="#ff6a00" stroke-width="1.7" />
+				<line y1="5.25" x2="18" y2="5.25" stroke="#ff6a00" stroke-width="1.7" />
+				<line y1="1.25" x2="18" y2="1.25" stroke="#ff6a00" stroke-width="1.7" />
+			</svg>
+		</div>
+	</button>
 
 	<MobileMenu bind:show={sidebar_show} />
 </div>
 
 <style>
 	.mobile_menu {
-		padding: 0 0.25rem;
+		padding: 0.5rem 1.5rem;
 		margin: 0;
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
-		background-color: #1a1a1b;
+		background-color: var(--background-color);
 	}
 	.logo {
 		color: white;
@@ -31,7 +46,17 @@
 		cursor: pointer;
 		padding: 0;
 		margin: 0;
-		padding: 1.5rem;
+		padding: 0.4rem;
+		border: none;
+		background-color: Transparent;
+	}
+	button:hover {
+		background-color: Transparent;
+		border: none;
+	}
+
+	button:active {
+		background-color: #a5a5a5;
 	}
 
 	@media (min-width: 768px) {
