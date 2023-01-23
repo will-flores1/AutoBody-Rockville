f1d9be58f04f9fc7c6bf99dca9b32c6d49cde3e3
wilfredo-flores <willflores1221@gmail.com>
2023-01-23 14:27:07 -0500

    add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [, svelte()]
+  integrations: [, svelte(), mdx()]
 });
